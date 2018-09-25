0686623e959b7c8aa2ca3024f9a8bbf8c31a5e22
sokhim eang <sokhimeang.bk@gmail.com>
2018-09-25 18:49:20 +0700

    Add student

---
+++ app/models/student.rb
@@ -0,0 +1,12 @@
+class Student < ApplicationRecord
+  has_many :feedbacks
+  has_many :class_students
+  has_one :oversee
+  has_many :invitations_to, class_name: Invitation.name,
+    foreign_key: :user_id, dependent: :destroy
+  has_many :exam_schedules, through: :invitations_to, source: :schedule
+  validates :code, presence: true
+  has_secure_password
+
+  scope :order_by_code, ->{order code :asc}
+end

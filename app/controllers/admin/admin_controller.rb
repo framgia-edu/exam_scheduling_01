5effbafb301ac326d2585af3129854a7fc785e03
trinhtan <lop12a1trinhvantan@gmail.com>
2018-09-26 08:59:01 +0700

    Init login

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,4 +1,4 @@
-class AdminController < ApplicationController
+class AdminController < Admin::BaseController
   def new; end
 
   def create; end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resource :subject, only: [:show]
+  get "/login", to: "login#new"
+  post "/login", to: "login#create"
 
+  resource :subject, only: [:show]
+  
   namespace :admin do
   	resource :subject, except: [:new, :create, :destroy]
   end 

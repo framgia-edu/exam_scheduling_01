f4c866405b213bcc4f8ffd64e01e79b49244b999
trinhtan <lop12a1trinhvantan@gmail.com>
2018-09-26 08:59:01 +0700

    Admin Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :subject, only: [:show]
+
+  namespace :admin do
+  	resource :subject, except: [:new, :create, :destroy]
+  end 
 end
